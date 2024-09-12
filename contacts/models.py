ee21738d9d90ca26e1b38ac795c9d672380074a8
RichardPeda <richard.peda@gmx.net>
2024-09-12 13:35:29 +0200

    new contact app

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    badge_color = models.CharField(max_length=20)
+    initials = models.CharField(max_length=2)
+    register = models.CharField(max_length=1)
+    selected = models.BooleanField(default=False)
